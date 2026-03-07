5c0e72d226c25557e063c1527845862ac6673bec
Somtochukwu Francis <somtofrancis5@gmail.com>
2026-03-08 00:26:44 +0100

    Minor

--- src/components/custom/SearchJobsTab.tsx
+++ src/components/custom/SearchJobsTab.tsx
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function SearchJobsTab() {
-  return <div>SearchJobsTab</div>;
+  return (
+    <div className="w-full justify-center items-center p-8">
+      <div className="border border-gray-200 rounded-md p-4 bg-purple-50 flex flex-col items-center justify-center gap-2">
+        <p className="font-bold text-gray-900">Nothing to see here yet.</p>
+        <p className="text-sm text-gray-500">
+          Currently fleshing out this tab. Try applying to a custom job instead.
+        </p>
+      </div>
+    </div>
+  );
 }
