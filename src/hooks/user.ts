01427830d3011ddba9ab1f44ff4eda31a1aa12d0
Somtochukwu Francis <somtofrancis5@gmail.com>
2026-04-06 21:48:02 +0100

    Minor

--- src/hooks/user.ts
+++ src/hooks/user.ts
@@ -5,7 +5,7 @@ import { queryKeys } from "../querykeyfactory";
 export function useFetchCurrentUser() {
     const queryKey = queryKeys.user.current;
 
-    const {data, isPending, isFetching, error} = useQuery({ queryKey, queryFn: async () => await getCurrentUser(), refetchInterval: 1000 * 60, refetchOnWindowFocus: true });
+    const {data, isPending, isFetching, error} = useQuery({ queryKey, queryFn: async () => await getCurrentUser(), refetchInterval: 1000 * 60, refetchOnWindowFocus: true, retry: 1 });
 
     return { user: data, isPending, isFetching, error, queryKey };
 }   
