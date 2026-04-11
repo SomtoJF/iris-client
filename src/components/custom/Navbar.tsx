2f526110fd46edac9d5a48b88b3ab4225df99fed
Somtochukwu Francis <somtofrancis5@gmail.com>
2026-04-11 11:28:48 +0100

    Extra stuff

--- src/components/custom/Navbar.tsx
+++ src/components/custom/Navbar.tsx
@@ -5,16 +5,19 @@ import { SettingsIcon } from "lucide-react";
 export default function Navbar() {
   const { user } = useUserStore();
   return (
-    <nav className="w-full border-b border-gray-200 flex items-center justify-end py-3.5 gap-4">
-      <Link
-        to="/settings/account"
-        className="text-sm flex no-wrap items-center text-muted-foreground hover:text-black"
-      >
-        <SettingsIcon className="w-4 h-4 mr-1" /> Settings
-      </Link>
-      <div className="w-8 h-8 text-white font-semibold bg-purple-600 rounded-md flex items-center justify-center">
-        {user?.firstName.charAt(0)}
-        {user?.lastName.charAt(0)}
+    <nav className="w-full border-b border-gray-200 flex items-center justify-between py-3.5 gap-4">
+      <img src="/logo-transparent.png" alt="Iris" className="h-12 w-auto" />
+      <div className="flex items-center gap-4">
+        <Link
+          to="/settings/account"
+          className="text-sm flex no-wrap items-center text-muted-foreground hover:text-black"
+        >
+          <SettingsIcon className="w-4 h-4 mr-1" /> Settings
+        </Link>
+        <div className="w-8 h-8 text-white font-semibold bg-purple-600 rounded-md flex items-center justify-center">
+          {user?.firstName.charAt(0)}
+          {user?.lastName.charAt(0)}
+        </div>
       </div>
     </nav>
   );
